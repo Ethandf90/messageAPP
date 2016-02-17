e46acc39868839030e111bc0da7be7b86610e2aa
Ethan Dong <ethan.dong@inventmode.com>
2016-02-17 15:21:20 -0500

    db url

--- server.js
+++ server.js
@@ -20,8 +20,8 @@ var app = express();
 
 // mongoose
 // mongoose.connect('mongodb://localhost/messageDB');
-mongoose.connect('mongodb://root:Jd9e5GSAsjmD@localhost/messageDB');
-// mongoose.connect('mongodb://root:Jd9e5GSAsjmD@ec2-107-23-47-197.compute-1.amazonaws.com:27017/messageDB');
+// mongoose.connect('mongodb://root:Jd9e5GSAsjmD@localhost/messageDB');
+mongoose.connect('mongodb://ethan:ethan@ec2-107-23-47-197.compute-1.amazonaws.com:27017/messageDB');
 
 // require routes for login api
 var routes = require('./app/loginApi.js');
